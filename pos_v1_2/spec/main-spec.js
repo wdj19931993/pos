6d3841d73c104dfdec0484bda84db37088245a96
wdj19931993 <645123704@qq.com>
2014-11-14 16:05:23 +0800

    version 1.1 "input is a single item"

--- pos_v1_2/spec/main-spec.js
+++ pos_v1_2/spec/main-spec.js
@@ -5,15 +5,15 @@ describe('pos', function () {
     beforeEach(function () {
         allItems = loadAllItems();
         inputs = [
-            'ITEM000001',
-            'ITEM000001',
-            'ITEM000001',
-            'ITEM000001',
-            'ITEM000001',
-            'ITEM000003-2',
-            'ITEM000005',
-            'ITEM000005',
-            'ITEM000005'
+            'ITEM000003',
+            // 'ITEM000001',
+            // 'ITEM000001',
+            // 'ITEM000001',
+            // 'ITEM000001',
+            // 'ITEM000003-2',
+            // 'ITEM000005',
+            // 'ITEM000005',
+            // 'ITEM000005'
         ];
     });
 
@@ -21,14 +21,15 @@ describe('pos', function () {
 
         spyOn(console, 'log');
 
-        printInventory([]);
+        printInventory(inputs);
 
         var expectText =
             '***<没钱赚商店>购物清单***\n' +
+            '名称：荔枝，数量：1斤，单价：15.00(元)，小计：15.00(元)\n' +
             '----------------------\n' +
             '挥泪赠送商品：\n' +
             '----------------------\n' +
-            '总计：0.00(元)\n' +
+            '总计：15.00(元)\n' +
             '节省：0.00(元)\n' +
             '**********************';
 
--- pos_v1_2/src/main.js
+++ pos_v1_2/src/main.js
@@ -1,17 +1,35 @@
 //TODO: Please write code in this file.
 
-function printInventory()
+function printInventory(inputs)
 {
+  var allItems = loadAllItems();
+
+  var items = createIndex(allItems);
+
+  var item = items[inputs[0]]
+
   var sum = 0;
   var saved = 0;
 	var s='***<没钱赚商店>购物清单***\n'
+    +'名称：'+ item.name +'，数量：1斤，单价：'
+    + item.price.toFixed(2) + '(元)，小计：15.00(元)\n' ;
 
 	s += '----------------------\n' +
 		 '挥泪赠送商品：\n' +
 	'----------------------\n' +
-	'总计：' + sum.toFixed(2) + '(元)\n' +
+	'总计：' + item.price.toFixed(2) + '(元)\n' +
 	'节省：'+ saved.toFixed(2) +'(元)\n' +
 	'**********************';
 
 	console.log(s);
+}
+
+function createIndex(allItems)
+{
+  var items = [];
+  for(item in allItems)
+  {
+    items[allItems[item].barcode] = allItems[item];
+  }
+  return items;
 }
